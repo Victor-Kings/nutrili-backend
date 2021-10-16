d64b7c51db0850f9c76da150c146fae3a3fc96c6
Christopher-OSouza <christopher.souza762@gmail.com>
2021-10-16 19:00:18 -0300

    fixing getnutritionist

--- src/main/java/com/nutrili/external/database/repository/NutritionistRepository.java
+++ src/main/java/com/nutrili/external/database/repository/NutritionistRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 import java.util.UUID;
 
 public interface NutritionistRepository extends JpaRepository<Nutritionist, UUID> {
-    @Query("select n from Nutritionist n join n.officeId a where a.city LIKE :city and a.city is not null order by n.score")
+    @Query("select n from Nutritionist n join n.officeId a where upper(a.city) LIKE upper(:city) and a.city is not null order by n.score")
     List<Nutritionist> findByCity(@Param("city") String city);
 
-    @Query("select n from Nutritionist n where n.name LIKE :name order by n.score")
+    @Query("select n from Nutritionist n where upper(n.name) LIKE upper(:name) order by n.score")
     List<Nutritionist> findByName(@Param("name") String name);
 
 }
